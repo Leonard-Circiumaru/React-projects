8f893aa1a128228a0e000768352d245f99fde29b
Leonard-Circiumaru <91908457+Leonard-Circiumaru@users.noreply.github.com>
2021-12-29 11:11:40 +0200

    react-basics-homework

---
+++ src/components/tourData.js
@@ -0,0 +1,38 @@
+export const tourData = [
+    {
+        id: 1,
+        city: "london",
+        img: "./img/london.jpg",
+        name: "big ben tour",
+        info: 
+        "London is the capital and largest city of England and the United Kingdom. It stands on the River Thames in south-east England at the head of a 50-mile (80 km) estuary down to the North Sea, and has been a major settlement for two millennia."
+    },
+
+    {
+        id: 2,
+        city: "paris",
+        img: "./img/paris.jpg",
+        name: "eiffel tower tour",
+        info: 
+        "Paris is the capital and most populous city of France, with an estimated population of 2,175,601 residents as of 2018, in an area of more than (41 square miles). Since the 17th century, Paris has been one of Europe's major centres of finance, diplomacy, commerce, fashion, gastronomy, science, and arts."
+    },
+
+    {
+        id: 3,
+        city: "berlin",
+        img: "./img/berlin.jpg",
+        name: "brandenburg gate tour",
+        info: 
+        "Berlin is the capital city of Germany and one of the 16 states of the Federal Republic of Germany. Berlin is the largest city in Germany and has a population of 4.5 million within its metropolitan area and 3.5 million from over 190 countries within the city limits."
+    },
+
+    {
+        id: 4,
+        city: "rome",
+        img: "./img/rome.jpg",
+        name: "colosseum tour",
+        info: 
+        "Rome (Italian and Latin: Roma is the capital city of Italy. It is also the capital of the region, the centre of the Metropolitan City of Rome. With 2,860,009 residents in 1,285 km Rome is the country's most populated and the third most populous city in the European Union by population within city limits."
+    }
+
+]
